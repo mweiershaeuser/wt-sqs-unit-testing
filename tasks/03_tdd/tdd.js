499d8dea4df3d49042ac2e38244c88cad0065876
Daniel Heuser <heuserda@icloud.com>
2022-05-09 20:17:28 +0200

    updated ToDos Task 03

--- tasks/03_tdd/tdd.js
+++ tasks/03_tdd/tdd.js
@@ -32,6 +32,10 @@ export function getWorkingTime(startTime, endTime, breaks = []) {
    * 1. Erstelle zwei Variablen, um die Anfangszeit und Endzeit seit Tagesbeginn in Minuten zu normalisieren. (HELP: Berechne die Stunden als Minuten)
    * 2. Erstelle eine IF-Bedingung, um zu überprüfen ob die Startzeit größer gleich der Endzeit ist und lass einen Fehler auswerfen, wenn die Bedingung erfüllt wird.
    * 3. Berechne die Minuten seit Tagesbeginn durch eine Substraktion der Anfangs- und Endzeit und halte diese. (HELP: Nutze dazu die Konstante result)
+   * 4. Erstelle eine IF-Bedingung, um zu überprüfen ob die Pausen (breaks) in der Summe größer als die bisherige Arbeitszeit (result) sind. Sollte der Fall eintreten, dann lass einen Fehler auswerfen. (HELP: Die Methode .reduce kann verwendet werden, um alle Elemente eines Arrays zu summieren)
+   * 5. Nun ist es an der Zeit die tatsächliche Arbeitszeit zu ermitteln, dazu kann man die Methode .forEach über die breaks iterieren lassen und die tatsächliche Arbeitszeit (result - breaks) erhalten.
+   * 6. Da zu Beginn der ToDos die Anfangszeit und Endzeit seit Tagesbeginn in Minuten normalisiert wurden, sollte nun die tatsächliche Arbeitszeit (result) wieder im Stunden-Format, durch eine Division, umgewandelt werden.
+   * Fertig, du hast es geschafft! Der Test sollte nun ohne Probleme durchlaufen. :)
    */
 
   return result;
