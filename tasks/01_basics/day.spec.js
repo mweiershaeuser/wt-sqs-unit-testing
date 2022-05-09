1bf66de8b246687eb30a28731e8379a6856430d4
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2022-05-09 23:03:41 +0200

    Update day.spec.js

--- tasks/01_basics/day.spec.js
+++ tasks/01_basics/day.spec.js
@@ -31,15 +31,15 @@ describe("Day", () => {
 
     /**
      * ToDo:
-     * 1. Erstelle eine Test Methode (it oder test), die testet, dass die Fetch Methode aus Zeile 20 mit einem vorgegebenem Datum aufgerufen werden sollte.
-     * 1.1 Teste darauf, ob der Tag (day) ein Feiertag (.isHolday()) ist und führe dann (.then) den Code im ToDo 1.2 aus. (HELP: Da der Test auf einer Fetch Methode durchgeführt wird verwenden wir anstatt des expect zuerst ein return, da dieses das Promise vom Fetch resolved.)
-     * 1.2 Innerhalb des .then erwarten (expect) wir, dass die Fetch Methode (HELP: globalThis.fetch) mit bspw. dem Datum 09.05.2022 aufgerufen (.toHaveBeenCalledWith) wird.
-     * 2. Erstelle eine weitere Test Methode, die testet, ob das vorgegebene Datum ein Feiertag ist und den Wert true returnt.
-     * 2.1 Speichere in die isHoliday Variable den Wert 1 als äquivalent für den Wert true.
-     * 2.2 Teste darauf, ob der Tag (day) ein Feiertag (.isHolday()) ist und führe dann (.then(h)) die Erwartung (expect) zur Überprüfung des Booleans (HELP: h) ob er wahr (.toBeTruthy()) ist aus.
-     * 3. Erstelle eine weitere Test Methode, die testet, ob das vorgegebene Datum ein Feiertag ist und den Wert false returnt.
+     * 1. Erstelle eine Test Methode (it oder test), die testet, dass die Fetch Methode aus Zeile 20 mit einem vorgegebenem Datum aufgerufen wird.
+     * 1.1 Führe die zu testende Methode (isHoliday()) des Testobjekts (day) aus und implementiere ToDo 1.2 im Callback (.then()). (HELP: Der durch die asynchrone Methode entstehende Promise sollte in der Testfunktion returned werden, damit Jest weiß, dass warten soll, bis der Promise resolved wird.)
+     * 1.2 Innerhalb des Callbacks erwarten (expect) wir, dass die Fetch Methode (HELP: globalThis.fetch) mit bspw. dem Datum 09.05.2022 in der URL aufgerufen (.toHaveBeenCalledWith) wird.
+     * 2. Erstelle eine weitere Test Methode, die testet, ob das vorgegebene Datum ein Feiertag ist und den Wert true returned.
+     * 2.1 Speichere in die isHoliday Variable den Wert 1 als Äquivalent für den Wert true.
+     * 2.2 Führe die zu testende Methode (isHoliday()) des Testobjekts (day) aus und implementiere dann (.then(h)) die Erwartung (expect) zur Überprüfung des Booleans (HELP: h) ob er wahr (.toBeTruthy()) ist.
+     * 3. Erstelle eine weitere Test Methode, die testet, ob das vorgegebene Datum ein Feiertag ist und den Wert false returned.
      * 3.1 Speichere in die isHoliday Variable den Wert 0 als äquivalent für den Wert false.
-     * 3.2 Teste darauf, ob der Tag (day) ein Feiertag (.isHolday()) ist und führe dann (.then(h)) die Erwartung (expect) zur Überprüfung des Booleans (HELP: h) ob er falsch (.toBeFalsy()) ist aus.
+     * 3.2 Führe die zu testende Methode (isHoliday()) des Testobjekts (day) aus und implementiere dann (.then(h)) die Erwartung (expect) zur Überprüfung des Booleans (HELP: h) ob er falsch (.toBeFalsy()) ist.
      */
   });
 });
